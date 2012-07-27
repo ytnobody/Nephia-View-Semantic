cd957e0d840e067c073caad7cba0e9e1bf4c344c
ytnobody <ytnobody@gmail.com>
2012-07-27 12:36:05 +0900

    initial commit

---
+++ lib/Nephia/View/Semantic.pm
@@ -0,0 +1,32 @@
+package Nephia::View::Semantic;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Nephia::View::Semantic -
+
+=head1 SYNOPSIS
+
+  use Nephia::View::Semantic;
+
+=head1 DESCRIPTION
+
+Nephia::View::Semantic is
+
+=head1 AUTHOR
+
+ytnobody E<lt>ytnobody@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
